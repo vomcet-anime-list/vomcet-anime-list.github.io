f285ff30387a8077564f1900aeef69bc9a8988fa
icanc <xiewenqi233@gmail.com>
2020-12-28 16:27:09 -0500

    now its dark

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 
 new gridjs.Grid({
     sort: true,
-    search: true,
-    pagination: false,
+    search: false,
+    pagination: true,
     columns: [
         { 
             id: 'image',
             name: 'Image',
             formatter: (cell) => gridjs.html(`<img class="max-h-20" src="${cell}">`),
-            width: "20px"
+            width: "30px"
         },
         { 
             id: 'name',
@@ -19,7 +19,24 @@ new gridjs.Grid({
         { 
             id: 'rating',
             name: 'Rating',
+            width: "30px"
         },
+        {
+            id: 'status',
+            name: 'status',
+            formatter: (cell, row) => {
+                if(row.cells[3].data == "finished"){
+                    return gridjs.h('a', {
+                        className: 'py-2 mb-4 px-4 border rounded-md text-white bg-green-600',
+                      }, 'Finished');
+                }else if(row.cells[3].data == "watching"){
+                    return gridjs.h('a', {
+                        className: 'py-2 mb-4 px-4 border rounded-md text-white bg-blue-600',
+                      }, 'Watching');
+                }
+                
+              }
+        }
      ],
     server: {
         url: 'src/anime.xml',
@@ -32,15 +49,20 @@ new gridjs.Grid({
               row.querySelector('image').innerHTML,
               row.querySelector('name').innerHTML,
               row.querySelector('rating').innerHTML,
+              row.querySelector('status').innerHTML,
             ]);
         }
       },
       className: {
-        table: ' antialiased font-sans font-bold text-lg text-gray-500',
-        th : '',
-        td : ' max-h-4',
-        //thead : 'w-full',
-        sort : 'box-border h-10 w-10 p-4 border-4',
-        search : 'shadow',
+        container : 'dark:bg-gray-800 dark:border-gray-900',
+        table: 'antialiased font-sans font-bold text-lg text-gray-500 dark:text-white dark:bg-gray-500 dark:border-gray-900',
+        th : 'dark:bg-gray-800 dark:text-white dark:border-gray-900',
+        td : 'dark:bg-gray-800 dark:border-gray-900',
+        thead : 'dark:bg-gray-500 dark:border-gray-900',
+        tbody : 'dark:border-gray-900',
+        //search : 'shadow',
+        paginationSummary  : 'dark:text-white dark:border-gray-900',
+        header : 'dark:border-gray-900',
+        footer  : 'dark:bg-gray-800 dark:border-gray-900'
       }
   }).render(document.getElementById("gridWrapper"));
